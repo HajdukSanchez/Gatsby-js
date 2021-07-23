4795d7c30ebc1a59bcd0f320eed7c791e90d022f
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-23 16:58:39 -0500

    :tada: [ADD] Start the project initial configuration, structure and dependencies

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\Platzi\\Frontend_ReactJS\\14-Gatsby-js\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("D:\\Platzi\\Frontend_ReactJS\\14-Gatsby-js\\src\\pages\\index.js"))
+}
+
